04e363dca5a75fda642c8e057df35009e3285fd5
Michael Kane Juncker <michael.juncker@enote.com>
2022-07-14 22:21:16 +0200

    chore(download): support user-provided file content type

--- server/routes/download.js
+++ server/routes/download.js
@@ -3,9 +3,15 @@ const storage = require('../storage');
 module.exports = async function(req, res) {
   const id = req.params.id;
   try {
+    const metadata = await storage.metadata(id).metadata;
+    const contentType =
+      metadata && metadata.contentType
+        ? metadata.contentType
+        : 'application/octet-stream';
+
     const { length, stream } = await storage.get(id);
     res.writeHead(200, {
-      'Content-Type': 'application/octet-stream',
+      'Content-Type': contentType,
       'Content-Length': length
     });
     stream.pipe(res);
